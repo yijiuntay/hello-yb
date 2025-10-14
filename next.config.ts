024abb02f1e541ad0bbc5a300946eb49c6179b1c
yijiuntay <yijiuntay@ymail.com>
2025-10-14 15:49:36 +0800

    attempt next.config.ts fix

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  experimental: {
-    // This tells Next.js to treat these packages as Node.js dependencies
-    // only to be included in the server environment (API Routes, Server Components).
-    serverComponentsExternalPackages: ["@prisma/client", "prisma"],
-  },
+  serverExternalPackages: ["@prisma/client", "prisma"],
 };
 
 export default nextConfig;
