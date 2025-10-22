7bb08ae3727289fe5ccdd7aac3c94ecd982ea703
yijiuntay <yijiuntay@ymail.com>
2025-10-23 04:57:03 +0800

    added new page to view and compare all constituencies

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,6 +1,6 @@
 // app/components/Header.tsx
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 
 const Header: React.FC = () => {
   return (
@@ -8,12 +8,18 @@ const Header: React.FC = () => {
       <div className="container mx-auto px-4 py-4 flex justify-between items-center">
         {/* Wrap the h1 with Link to navigate to the homepage ("/") */}
         <Link href="/">
-          <h1 className="text-xl md:text-2xl text-yellow-300 cursor-pointer" style={{ textShadow: '2px 2px 0px #000' }}>
+          <h1
+            className="text-xl md:text-2xl text-yellow-300 cursor-pointer"
+            style={{ textShadow: "2px 2px 0px #000" }}
+          >
             Hello YB
           </h1>
         </Link>
         <nav>
-          <a href="/constituencies" className="text-white hover:text-yellow-300 transition-colors">
+          <a
+            href="/overview"
+            className="text-white hover:text-yellow-300 transition-colors"
+          >
             Constituencies
           </a>
         </nav>
